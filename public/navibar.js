c3b9b0f5873d770858f7c405052460ba6647d12c
julsamson <142834185+julsamson@users.noreply.github.com>
2023-10-07 18:49:49 +0300

    Update navibar.js

--- public/navibar.js
+++ public/navibar.js
@@ -1,7 +1,6 @@
 const bars = document.getElementById('bars');
 const xmark = document.getElementById('xmark');
 const nav = document.getElementById('naviTab')
-const navLink = document.querySelectorAll('div ul li a');
 
 bars.addEventListener('click', function (){
     bars.classList.toggle('hidden');
@@ -12,6 +11,4 @@ xmark.addEventListener('click', function(){
     bars.classList.remove('hidden');
     nav.classList.add('hidden');
 })
-navLink.addEventListener('click', function(){
-    nav.classlist.add('hidden');
-})
+
